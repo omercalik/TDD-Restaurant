66aa5cefd56d7e0f003e71b0adc435340ba2705d
omercalik <omerbcalik@gmail.com>
2022-02-11 00:00:31 +0300

    Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,9 @@
-import React from "react"
+import React, { useEffect } from "react"
 
-const RestaurantList = () => {
+export const RestaurantList = ({ loadRestaurants }) => {
+  useEffect(() => {
+    loadRestaurants()
+  }, [loadRestaurants])
   return <div>RestaurantList</div>
 }
 
