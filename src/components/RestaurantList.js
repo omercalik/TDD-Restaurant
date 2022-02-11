1b781f28efb555a09d2a10b0da39b23799e043b3
omercalik <omerbcalik@gmail.com>
2022-02-11 17:21:51 +0300

    Add restaurants as prop to RestaurantList

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react"
 
-export const RestaurantList = ({ loadRestaurants }) => {
+export const RestaurantList = ({ loadRestaurants, restaurants }) => {
   useEffect(() => {
     loadRestaurants()
   }, [loadRestaurants])
-  return <div>RestaurantList</div>
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  )
 }
 
 export default RestaurantList
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -4,8 +4,29 @@ import { RestaurantList } from "../RestaurantList"
 describe("RestaurantList", () => {
   it("loads restaurants on first render", () => {
     const loadRestaurants = jest.fn().mockName("loadRestaurants")
-    render(<RestaurantList loadRestaurants={loadRestaurants} />)
+    const restaurants = []
+    render(
+      <RestaurantList
+        restaurants={restaurants}
+        loadRestaurants={loadRestaurants}
+      />
+    )
 
     expect(loadRestaurants).toHaveBeenCalled()
+  })
+
+  it("displays the restaurans", () => {
+    const noop = () => {}
+    const restaurants = [
+      { id: 1, name: "Sushi Place" },
+      { id: 2, name: "Pizza Place" },
+    ]
+
+    const { queryByText } = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />
+    )
+
+    expect(queryByText("Sushi Place")).not.toBeNull()
+    expect(queryByText("Pizza Place")).not.toBeNull()
   })
 })
